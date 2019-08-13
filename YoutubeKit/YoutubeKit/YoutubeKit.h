44fd676592b326ffccb310d2cc6fff373279ffa6
Nguyen Thanh Bình <binhvuong.2010@gmail.com>
2019-08-14 01:33:16 +0700

    Initial project setup

---
+++ YoutubeKit/YoutubeKit/YoutubeKit.h
@@ -0,0 +1,19 @@
+//
+//  YoutubeKit.h
+//  YoutubeKit
+//
+//  Created by Nguyen Thanh Bình on 8/14/19.
+//  Copyright © 2019 Nguyen Thanh Bình. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YoutubeKit.
+FOUNDATION_EXPORT double YoutubeKitVersionNumber;
+
+//! Project version string for YoutubeKit.
+FOUNDATION_EXPORT const unsigned char YoutubeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YoutubeKit/PublicHeader.h>
+
+
